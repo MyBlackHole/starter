406f8cc21a8a96906654ce37a1cbe9fa70177f75
black <1358244533@qq.com>
2024-08-04 08:12:27 +0800

    init

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,79 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- 写入文件的编码
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8,gbk"
+vim.opt.fileformats = "unix"
+vim.auto_complete = true
+-- -- 搜索到文件两端时不重新搜索
+-- vim.opt.nowrapscan = true
+-- 允许neovim 访问系统剪贴板
+vim.opt.clipboard = "unnamedplus"
+-- 突出显示搜索模式的所有匹配项
+vim.opt.hlsearch = true
+-- 创建备份文件
+vim.opt.backup = false
+-- 总是显示标签
+vim.opt.showtabline = 2
+-- 设置编号行
+vim.opt.number = true
+-- 高亮当前行
+vim.opt.cursorline = true
+-- 将行显示为一长行
+vim.opt.wrap = false
+-- 移动光标时保留上下左右指定行列
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+-- 将制表符转换为空格
+vim.opt.expandtab = true
+-- 如果一个文件正在被另一个程序编辑（或在另一个程序编辑时被写入文件），则不允许编辑它
+vim.opt.writebackup = false
+-- 启用持久撤销
+vim.opt.undofile = true
+-- 创建交换文件
+vim.opt.swapfile = false
+-- 再次让缩进更智能
+vim.opt.smartindent = true
+-- 智能案例
+vim.opt.smartcase = true
+-- 换行缩进
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- 自动缩进:产考上一行
+vim.opt.autoindent = true
+-- 使用C/C++语言自动缩进
+vim.opt.cindent = true
+-- 关闭鼠标
+vim.opt.mouse = ""
+-- 开启光标所在行相对行号
+vim.opt.relativenumber = true
+-- 命令行高度
+vim.opt.cmdheight = 1
+-- 启用语法高亮
+vim.opt.syntax = "enable"
+-- 按语法高亮折叠
+-- vim.opt.foldmethod = "indent"
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+-- 显示所有符号
+vim.opt.list = true
+-- 光标在行首的时候，使用左右方向键可以跳转到上一行或下一行
+vim.opt.whichwrap = "<,>,[,]"
+-- 当文件被外部程序修改的时候 自动加载修改后的内容
+vim.g.autoread = true
+vim.bo.autoread = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- 自动操作
+vim.api.nvim_create_autocmd(
+    "BufWinEnter",
+    {
+        pattern = { "*.py", "*.c", "*.h", "*.cpp", "Makefile", "*.s", "*.ld", "*.S", "*.mk", "*.html", "*.md" },
+        -- enable wrap mode for json files only
+        command = "%s/[\\u0d]//ge",
+    }
+)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,173 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "c",
+        "cpp",
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    opts = function()
+      local conf = require "nvchad.configs.telescope"
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+      conf.defaults.mappings.i = {
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<Esc>"] = require("telescope.actions").close,
+      }
+
+      -- or
+      -- table.insert(conf.defaults.mappings.i, your table)
+
+      return conf
+    end,
+  },
+  { "mrjones2014/nvim-ts-rainbow" },
+  -- 翻译
+  {
+    "voldikss/vim-translator",
+    keys = {
+      {
+        "<leader>tt",
+        mode = {"v"},
+        "<cmd>Translate<cr>",
+        desc = "Translate",
+      },
+      {
+        "<leader>tr",
+        mode = {"v"},
+        "<cmd>TranslateR<cr>",
+        desc = "TranslateR",
+      },
+      {
+        "<leader>tl",
+        mode = {"v"},
+        "<cmd>TranslateL<cr>",
+        desc = "TranslateL",
+      },
+      {
+        "<leader>th",
+        mode = {"v"},
+        "<cmd>TranslateH<cr>",
+        desc = "TranslateH",
+      },
+      {
+        "<leader>tw",
+        mode = {"v"},
+        "<cmd>TranslateW<cr>",
+        desc = "TranslateW",
+      },
+      {
+        "<leader>tx",
+        mode = {"v"},
+        "<cmd>TranslateX<cr>",
+        desc = "TranslateX",
+      },
+    },
+  },
+  -- 跳跃定位
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
+  {
+    "Mythos-404/xmake.nvim",
+    lazy = true,
+    event = "BufReadPost xmake.lua",
+    config = true,
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  },
+  -- :Fitten login BlackHole 1358
+  {
+    "luozhiya/fittencode.nvim",
+    config = function()
+      require("fittencode").setup()
+    end,
+  },
 }
