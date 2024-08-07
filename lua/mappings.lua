320bec82babe9aa2fe2e19470aac0484d7b3bb61
black <1358244533@qq.com>
2024-08-07 09:59:17 +0800

    修改默认的影射键

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,12 +3,37 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map('n', 'grn', vim.lsp.buf.rename)
-map('n', 'gra', vim.lsp.buf.code_action)
-map('n', 'grr', vim.lsp.buf.references)
-map('i', '<C-s>', vim.lsp.buf.signature_help)
+-- 删除默认配置的代码树打开影射
+unmap("n", "<leader>e")
+unmap("n", "<leader>x")
+unmap("n", "<leader>b")
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- 使 ; 同步为 :
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+-- lsp 重命名
+map("n", "grn", vim.lsp.buf.rename)
+map("n", "gra", vim.lsp.buf.code_action)
+map("n", "grr", vim.lsp.buf.references)
+map("i", "<C-s>", vim.lsp.buf.signature_help)
+
+map("n", "H", function()
+  require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "L", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+-- buffer 管理
+map("n", "<leader>bx", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
+
+map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "buffer new" })
+
